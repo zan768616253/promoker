ea58bc8474f0bd45745ac2aebb2885bc6e8a8698
yaoyi <linyaoyi011@gmail.com>
2014-06-20 00:38:48 +0800

    update faye deploy codes

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,7 +71,7 @@ namespace :faye do
   	on release_roles :all do
 	  	if test "[ -e #{current_path}/tmp/pids/faye.pid ]"
 	        within current_path do
-	        	execute :kill, "$(cat tmp/pids/faye.pid && rm tmp/pids/faye.pid"
+	        	execute :kill, "$(cat tmp/pids/faye.pid) && rm tmp/pids/faye.pid"
 	        end
 	    end
 	end
